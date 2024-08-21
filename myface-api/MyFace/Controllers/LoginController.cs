6bab0bb697da60614e3f4f44bbee86ff36a6fc76
Purbai <purbai.varsani@softwire.com>
2024-08-21 17:28:12 +0100

    extracted the user /pwd from request header

--- myface-api/MyFace/Controllers/LoginController.cs
+++ myface-api/MyFace/Controllers/LoginController.cs
@@ -25,15 +25,30 @@ namespace MyFace.Controllers
         //if 401 response, remove user's details from context and display this page again
         [HttpPost("/login")]
         [AllowAnonymous]
-        public IActionResult Login([FromBody] UserLoginRequest loginUser)
+        public IActionResult Login()
         {
-            Console.WriteLine($"inside the Login controller - user = {loginUser.Username}, pass = {loginUser.Password}");
+            // get the Authorization part of the request
+            string authHeader = HttpContext.Request.Headers["Authorization"];
+
+
+            string username = "";
+            string passwd = "";
+            // remove the 'Basic' from the Authorization string
+            string encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
+            // decode
+            System.Text.Encoding encoding = System.Text.Encoding.GetEncoding("iso-8859-1");
+            string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
+            // now extract the username and password 
+            int seperatorIndex = usernamePassword.IndexOf(':');
+                username = usernamePassword.Substring(0, seperatorIndex);
+                passwd = usernamePassword.Substring(seperatorIndex + 1);
+
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-
-            var user = _usersRepo.Authenticate(loginUser.Username, loginUser.Password);
+            // pass to get validated
+            var user = _usersRepo.Authenticate(username, passwd);
             if (user == null)
             {
                 return BadRequest(ModelState);
